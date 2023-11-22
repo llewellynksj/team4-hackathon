3f1ea0fde71bc2fa027fb5f10fce2c64406ac3d5
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-22 19:25:11 +0000

    Add basic styles to auth login, signup pages

--- hack_team_4/settings.py
+++ hack_team_4/settings.py
@@ -148,8 +148,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
