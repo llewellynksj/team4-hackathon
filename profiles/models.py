05161de9c3326270bf234b2a8a5e7eec8bc64f1d
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-23 15:27:11 +0000

    Amended function to automatically create a profile when a user is created

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
@@ -10,7 +11,7 @@ class Profile(models.Model):
   """
   created_on = models.DateTimeField(auto_now_add=True)
   username = models.OneToOneField(User, on_delete=models.CASCADE)
-  pseudonym = models.CharField(max_length=100, help_text='Enter the name you would like to be known as to ALL users')
+  pseudonym = models.CharField(max_length=100, help_text='Enter the name you would like to be known as to ALL users', default='pseudonym')
   profile_pic = CloudinaryField('image', default='placeholder_k3nekm')
   first_name = models.CharField(max_length=100, null=True, blank=True)
   last_name = models.CharField(max_length=100, null=True, blank=True)
@@ -21,14 +22,13 @@ class Profile(models.Model):
     ordering = ['-created_on']
   
   def __str__(self):
-    return f"{self.user}"
+    return f"{self.username}"
   
+  @receiver(post_save, sender=User)
   def create_profile(sender, instance, created, **kwargs):
-    """
-    Function to create a profile automatically when a User is created
-    """
+    """Function to automatically create a profile when a user is created"""
     if created:
-        Profile.objects.create(username=instance)
+      Profile.objects.create(username=instance)
   
 
 TAG_CATEGORIES = [
