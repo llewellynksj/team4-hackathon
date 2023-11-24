da546bf424e7e7643df0b1f1d77cbe2b09b8b724
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-11-24 09:32:01 +0000

    Add view, url and template to display all profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
   path('<int:pk>/profiledetail/', views.DisplayProfile.as_view(), name='profile'),
   path(
-    'register/',
-    views.Registration.as_view(),
-    name='register'),
-  path('update_profile/', views.UpdateProfile.as_view(), name='update_profile')
+    'register/', views.Registration.as_view(), name='register'),
+  path('profile_list/', views.ProfileList.as_view(), name='profile_list'),
 ]
